3a811331444a01118b8ec9bd8ccafd69b872f3db
Abhishek Sharma <abhi.asno1@gmail.com>
2023-01-30 00:04:23 +0530

    added icon in head

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,14 @@ import { data } from "../lib/data";
 
 
 export default function Home() {
+  const products = [...data.products]
 
   return (
     <>
       <Layout>
         <div className="home-main p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {
-            data.products.map((product) => {
+            products.map((product) => {
               //console.log(product)
               return (
                 <Product key={product.id} productData={product} />
